b8a427c6500ebcb62340362e7b72b29e1f87d84a
Ryan Brue <ryanbrue.dev@gmail.com>
2024-09-22 14:44:40 -0500

    eh

--- cosmic-applet-quick-settings/src/main.rs
+++ cosmic-applet-quick-settings/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn main() -> cosmic::iced::Result {
+    tracing_subscriber::fmt::init();
+    let _ = tracing_log::LogTracer::init();
+
+    tracing::info!("Starting quick settings applet with version {VERSION}");
+
+    cosmic_applet_quick_settings::run()
 }
